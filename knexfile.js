9fb81b23fe782c42eedf7f184addb7214239c3cf
Joey Nevarez <josephnevarez12@gmail.com>
2020-03-05 17:31:13 -0800

    everything before writing the migration

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,17 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    }, 
+    migrations: {
+      directory: './migrations'
     },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
